c82f0c92aeade18902ce207aab47de96af9d0d24
wesle <wesle@Wesley-PC>
2026-09-17 14:26:06 -0300

    feat: inicia projeto OfiTech com Spring Boot e entidade Cliente

---
+++ ofitech/src/test/java/com/ofitech/OfitechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ofitech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfitechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
